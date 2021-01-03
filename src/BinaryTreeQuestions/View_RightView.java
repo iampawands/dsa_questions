67b80d647382aa2bbc8001f40f68cd989020722d
iampawands <iam.pawands@gmail.com>
2021-01-03 12:11:46 +0530

    top,bottom,left and right view of binary tree

---
+++ src/BinaryTreeQuestions/View_RightView.java
@@ -0,0 +1,71 @@
+package BinaryTreeQuestions;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+// Main driver class
+public class View_RightView {
+	static int maxLevel = Integer.MIN_VALUE;
+   	static List<Integer> rightView = new ArrayList();
+	// Tree node class
+	static class Node {
+		int data; // data of the node
+		int hd; // horizontal distance of the node
+		Node left, right; // left and right references
+
+		public Node(int key) {
+			data = key;
+			hd = Integer.MAX_VALUE;
+			left = right = null;
+		}
+	}
+
+	// Tree class
+	static class Tree {
+		Node root; // root node of tree
+		// Default constructor
+
+		public Tree() {
+		}
+
+		// Parameterized tree constructor
+		public Tree(Node node) {
+			root = node;
+		}
+
+		// Method that prints the bottom view.
+		public void leftView() {
+			Node node = root;
+			leftViewUtil(node,0);
+			for(int i:rightView) {
+				System.out.print(i+" ");
+			}
+		}
+		
+		private void leftViewUtil(Node node, int level) {
+			if(node==null) return;
+			
+			if(level>maxLevel) {
+				rightView.add(node.data);
+				maxLevel = level;
+			}
+			leftViewUtil(node.right, level+1);
+			leftViewUtil(node.left, level+1);
+		}
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(20);
+		root.left = new Node(8);
+		root.right = new Node(22);
+		root.left.left = new Node(5);
+		root.left.right = new Node(3);
+		root.right.left = new Node(4);
+		root.right.right = new Node(25);
+		root.left.right.left = new Node(10);
+		root.left.right.right = new Node(14);
+		Tree tree = new Tree(root);
+		System.out.println("Right view of the given binary tree:");
+		tree.leftView();
+	}
+}
---
+++ src/BinaryTreeQuestions/View_TopView.java
@@ -0,0 +1,92 @@
+package BinaryTreeQuestions;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+import BinaryTreeQuestions.View_BottomView.Node;
+
+
+
+// Main driver class
+public class View_TopView {
+	
+	// Tree node class
+	static class Node {
+		int data; // data of the node
+		int hd; // horizontal distance of the node
+		Node left, right; // left and right references
+
+		public Node(int key) {
+			data = key;
+			hd = Integer.MAX_VALUE;
+			left = right = null;
+		}
+	}
+
+	//Tree class
+	static class Tree {
+		Node root; // root node of tree
+	// Default constructor
+
+		public Tree() {
+		}
+
+	// Parameterized tree constructor
+		public Tree(Node node) {
+			root = node;
+		}
+
+	// Method that prints the bottom view.
+		public void topView() {
+
+			Queue<Node> q  = new LinkedList<Node>();
+			Map<Integer,Integer> m = new TreeMap();
+			Node node = root;
+			node.hd=0;
+			q.add(node);
+			while(!q.isEmpty()) {
+				Node t = q.poll();
+				int hd = t.hd;
+				
+				if(!m.containsKey(hd)) {
+					m.put(hd, t.data);
+				}
+				
+//				if(m.getOrDefault(hd, null)==null) {
+//					m.put(hd, t.data);
+//				}
+				if(t.left!=null) {
+					t.left.hd = hd-1;
+					q.add(t.left);
+				}
+				if(t.right!=null) {
+					t.right.hd=hd+1;
+					q.add(t.right);
+				}
+			}
+			
+			Set s = m.entrySet();
+			Iterator it = s.iterator();
+			while(it.hasNext()) {
+				Entry entry = (Entry) it.next();
+				System.out.print(entry.getValue()+" ");
+			}
+		
+		}
+	}
+	
+	public static void main(String[] args) {
+		Node root = new Node(20);
+		root.left = new Node(8);
+		root.right = new Node(22);
+		root.left.left = new Node(5);
+		root.left.right = new Node(3);
+		root.right.left = new Node(4);
+		root.right.right = new Node(25);
+		root.left.right.left = new Node(10);
+		root.left.right.right = new Node(14);
+		Tree tree = new Tree(root);
+		System.out.println("Top view of the given binary tree:");
+		tree.topView();
+	}
+}
